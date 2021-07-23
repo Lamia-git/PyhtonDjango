a7b4d99df8cb9dcd9bdd106eb5bbfb8b7a44079e
lamia ADJIR <lamia.kaciaissa@gmail.com>
2021-07-23 16:47:01 +0200

    added token

--- profiles_app/views.py
+++ profiles_app/views.py
@@ -7,6 +7,8 @@ from rest_framework.response import Response
 from rest_framework import status, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 from profiles_app import persmissions
 from profiles_app import serializers
 from profiles_app import models
@@ -112,6 +114,6 @@ class userProfileViewSet(viewsets.ModelViewSet):
     search_fields = ('name','email')
     
 
-
-
-
+class UserLoginApiView(ObtainAuthToken):
+    """handle creating user authentification tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
